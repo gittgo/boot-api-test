3368c7cbb241b86baeb3abcdd169f0ce1d53d31b
Administrator <332860608@qq.com>
2019-04-11 23:47:29 +0800

    NIO 服务端

--- src/main/java/com/example/bootapitest/netty/day03NIO/MultiplexerTimeServer.java
+++ src/main/java/com/example/bootapitest/netty/day03NIO/MultiplexerTimeServer.java
@@ -2,18 +2,16 @@ package com.example.bootapitest.netty.day03NIO;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
 
 public class MultiplexerTimeServer implements Runnable{
-
-
-
-
-
-
-
 
 
 
@@ -52,9 +50,89 @@ public class MultiplexerTimeServer implements Runnable{
      */
     @Override
     public void run() {
-        while (!stop){
 
+
+        while (!stop){
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectionKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()){
+                    key = it.next();
+                    it.remove();
+                    try {
+                        handleInput(key);
+                    }catch (Exception e){
+                        if (key != null){
+                            key.cancel();
+                            if(key.channel() != null)
+                                key.channel().close();
+                        }
+                    }
+                }
+            }catch (Throwable e){
+                e.printStackTrace();
+            }
+// 多路复用器关闭后,所有注册在上面的Channel和Pipe等资源都会被自动注册并关闭,所以不需要重复释放资源
         }
 
     }
+
+    private void handleInput(SelectionKey key) throws IOException{
+
+
+
+
+
+
+
+        if (key.isValid()){
+            if (key.isAcceptable()){
+                //Accept the new connection
+                ServerSocketChannel ssc = (ServerSocketChannel)key.channel();
+                SocketChannel sc = ssc.accept();
+                sc.configureBlocking(false);
+                // Add the new connection to the selector
+                sc.register(selector,SelectionKey.OP_READ);
+            }
+            if (key.isReadable()){
+                // Read the data
+                SocketChannel sc = (SocketChannel) key.channel();
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(readBuffer);
+                if(readBytes > 0){
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes,"UTF-8");
+                    System.out.println("The time server receive order  : "+ body);
+                    String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)?new java.util.Date(System.currentTimeMillis()).toString():"BAD ORDER";
+                    doWrite(sc,currentTime);
+                }else if (readBytes<0){
+                    // 对端链路关闭
+                    key.cancel();
+                    sc.close();
+                }else {
+
+                }
+            }
+        }
+    }
+
+
+
+
+    private void doWrite(SocketChannel channel,String response)throws IOException{
+        if(response != null && response.trim().length() > 0){
+            byte[] bytes = response.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+            channel.write(writeBuffer);
+        }
+    }
+
+
+
 }
