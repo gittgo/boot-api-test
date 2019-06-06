976c48f6d0193be4bf012975529ab0c93e0a39ba
wangchengwei <wangchengwei@tcps.com.cn>
2019-06-06 17:25:12 +0800

    netty NIO初稿

---
+++ src/main/java/com/example/bootapitest/netty/day05nettyTimeserver/TimeServerHandler.java
@@ -0,0 +1,14 @@
+package com.example.bootapitest.netty.day05nettyTimeserver;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+/**
+ * @author wang cheng wei
+ * @date 2019-5-31 15:06
+ */
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+
+
+
+}
