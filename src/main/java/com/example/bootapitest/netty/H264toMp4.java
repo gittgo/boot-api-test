65a9bbe179d44df4eeb83556439ef69dfe81daef
wangchengwei <wangchengwei@tcps.com.cn>
2019-12-06 11:35:08 +0800

    h264测试

---
+++ src/main/java/com/example/bootapitest/netty/H264toMp4.java
@@ -0,0 +1,33 @@
+package com.example.bootapitest.netty;
+
+import com.alibaba.dubbo.common.json.JSONObject;
+
+import java.io.File;
+import com.streamax.mp4.H264ToMp4;
+/**
+ * @author wang cheng wei
+ * @date 2019-11-27 14:14
+ */
+public class H264toMp4 {
+
+    public static void main(String[] args) {
+
+        JSONObject video = new JSONObject();
+//        String h264FilePath ="C:\\Users\\admin\\Desktop\\02_09_6501_0_1574661055474.h264";
+        String h264FilePath ="D:\\work\\idea\\ideawork\\netty\\boot-api-test\\src\\main\\java\\com\\example\\bootapitest\\netty\\02_09_6501_0_1574661055474.h264";
+        String mp4FilePath = h264FilePath.replaceAll("h264", "mp4");
+//        h264FilePath = h264FilePath.replaceAll("C:/Users/admin/Desktop/", "C:/Users/admin/Desktop/");
+//        mp4FilePath = mp4FilePath.replaceAll("C:/Users/admin/Desktop/", "C:/Users/admin/Desktop/");
+//        File h264File = new File(h264FilePath);
+//        File mp4File = new File(mp4FilePath);
+//        if (h264File.exists() && !mp4File.exists() && h264File.getName().contains("h264")) {
+
+
+            H264ToMp4 h264ToMp4 = new H264ToMp4();
+            int res = h264ToMp4.Converth264ToMp4(h264FilePath, "D:\\work\\idea\\ideawork\\netty\\boot-api-test\\src\\main\\java\\com\\example\\bootapitest\\netty\\02_09_6501_0_157466105555.mp4");
+//        }
+//        String videoPath = constant.videoIpPath + basicParametersDao.getAbsolutePath() + alarmFile.getFilePath().replaceAll("h264", "mp4");
+//        videoArray.add(videoPath);
+
+    }
+}
